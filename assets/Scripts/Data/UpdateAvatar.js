75d8d99e65982b4692f97ff0baf244d403e1c04e
ericwalkerw <ericwalker@gmail.com>
2023-12-11 17:59:40 +0700

    add menu

--- assets/Scripts/Data/UpdateAvatar.js
+++ assets/Scripts/Data/UpdateAvatar.js
@@ -7,27 +7,13 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-const randomWords = require('random-words');
-cc.Class({
-    extends: cc.Component,
 
-    properties: {
-        textQuest:cc.Label,
-        inputText:cc.EditBox,
-    },
-    start () {
-        this.createWords();
-    },
-    createWords(){
-        const words = randomWords(1).join('');
-        this.textQuest.string = words;
-    },
-    onTextChange(){
-        if(this.inputText.string[this.inputText.string.length - 1] == " "){
-            this.createWords();
-            this.inputText.string = '';
-            this.inputText.blur();
-            this.inputText.focus();
-        }
-    },
+cc.Class({
+  extends: cc.Component,
+  properties: {
+    avatar:cc.Sprite,
+  },
+  setAvatar(img){
+    this.avatar.spriteFrame = img;
+  }
 });
