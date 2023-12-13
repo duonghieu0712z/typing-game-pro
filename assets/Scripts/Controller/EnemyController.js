eed3147426827fabd4c68bb35ec49677ad426555
Duong Hieu <duonghieu0712z@gmail.com>
2023-12-13 17:09:14 +0700

    Fix enemy move

--- assets/Scripts/Controller/EnemyController.js
+++ assets/Scripts/Controller/EnemyController.js
@@ -1,39 +1,54 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 const Emitter = require("EventEmitter");
 const EventCode = require("EventCode");
+
 cc.Class({
-  extends: cc.Component,
+    extends: cc.Component,
 
-  properties: {
-    spine: sp.Skeleton,
-    speed: 2,
-    isAttack:false,
-  },
+    properties: {
+        spine: sp.Skeleton,
+        speed: 2,
+        isAttack: false,
+    },
 
-  moving(target) {
-    const s = Math.abs(target.x - this.node.x);
-    cc.tween(this.node)
-      .to(s/this.speed, { position: cc.v2(target.x, this.node.y) })
-      .start();
-      cc.log(s);
-    if(s <= 100 && !this.isAttack){
-      this.attack();
-      this.isAttack = true;
-    }
-  },
+    moving(target) {
+        const action = cc
+            .spawn(
+                cc.callFunc(() => this.moveAction(target)),
+                cc.delayTime(1)
+            )
+            .repeatForever();
+        this.node.runAction(action);
+    },
 
-  attack() {
-    this.spine.setAnimation(0, "Attack_1", 0);
-    this.spine.setCompleteListener(() => {
-      Emitter.instance.emit(EventCode.PLAYER_DIE);
-    });
-  },
+    moveAction(target) {
+        const direction = target.position.sub(this.node.position);
+        const velocity = direction.normalize().mul(this.speed);
+        this.increaseSpeed();
+
+        const move = cc.sequence(
+            cc.moveBy(1, velocity),
+            cc.callFunc(() => {
+                this.node.stopAction(move);
+                const s = Math.abs(target.x - this.node.x);
+                if (s <= 100 && !this.isAttack) {
+                    this.attack();
+                    this.node.stopAllActions();
+                }
+            })
+        );
+
+        this.node.runAction(move);
+    },
+
+    attack() {
+        this.spine.setAnimation(0, "Attack_1", 0);
+        this.spine.setCompleteListener(() => {
+            Emitter.instance.emit(EventCode.PLAYER_DIE);
+        });
+        this.isAttack = true;
+    },
+
+    increaseSpeed() {
+        this.speed++;
+    },
 });
--- assets/Scripts/Manager/GameManager.js
+++ assets/Scripts/Manager/GameManager.js
@@ -2,27 +2,23 @@ const Emitter = require("EventEmitter");
 const EventCode = require("EventCode");
 
 cc.Class({
-  extends: cc.Component,
+    extends: cc.Component,
 
-  properties: {
-    player: cc.Node,
-    enemy: cc.Node,
-  },
-  onLoad() {
-    Emitter.instance.registerEvent(
-      EventCode.PLAYER_MOVING,
-      this.playerMove.bind(this)
-    );
-    Emitter.instance.registerOnce(
-      EventCode.ENEMY_MOVING,
-      this.EnemyMove.bind(this)
-    );
-  },
-  playerMove() {
-    this.player.getComponent('PlayerController').moving();
-    this.EnemyMove();
-  },
-  EnemyMove() {
-    this.enemy.getComponent('EnemyController').moving(this.player);
-  },
+    properties: {
+        player: cc.Node,
+        enemy: cc.Node,
+    },
+
+    onLoad() {
+        Emitter.instance.registerEvent(EventCode.PLAYER_MOVING, this.playerMove.bind(this));
+        Emitter.instance.registerOnce(EventCode.ENEMY_MOVING, this.EnemyMove.bind(this));
+    },
+
+    playerMove() {
+        this.player.getComponent("PlayerController").moving();
+    },
+
+    EnemyMove() {
+        this.enemy.getComponent("EnemyController").moving(this.player);
+    },
 });
